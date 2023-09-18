64abcc3debf07c5a41eeb801b9f224f9f5242137
tobbesoren <113234609+tobbesoren@users.noreply.github.com>
2023-09-18 17:25:51 +0200

    moneyBeggingForToys

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <ParentComponent/>
+    </>
+  )
+}
+
+export default App
+
+
+const ParentComponent = () => {
+  const [savings, setSavings] = useState(250);
+  const moneyNeeded = 100;
+
+  return (
+    <>
+      Parents savings: {savings} $
+      <br/>
+      <br/>
+      Money needed for toy: {moneyNeeded} $
+      <br/>
+      <br/>
+      <ChildComponent savings={savings} moneyNeeded={moneyNeeded} setSavings={setSavings}/>
+      
+    </>
+  )
+}
+
+
+const ChildComponent = (props) => {
+  const [childMoney, setChildMoney] = useState(0);
+  const [numberOfToys, setNumberOfToys] = useState(0);
+
+  const askForMoney = () => {
+    if (props.savings >= props.moneyNeeded) {
+    props.setSavings(props.savings - props.moneyNeeded);
+    setChildMoney(childMoney + props.moneyNeeded);
+    }
+  }
+
+const buyToy = () => {
+  if (childMoney >= props.moneyNeeded) {
+    setChildMoney(childMoney - props.moneyNeeded);
+    setNumberOfToys(numberOfToys + 1);
+  }
+}
+
+  return (
+    <>
+      Child's money: {childMoney} $
+      <br/>
+      Number of toys: {numberOfToys}
+      <br/>
+      
+      <br/>  
+      <button onClick={askForMoney}>Ask for money</button>
+      <br/>
+      <button onClick={buyToy}>Buy toy</button>
+    </>
+  )
+}
